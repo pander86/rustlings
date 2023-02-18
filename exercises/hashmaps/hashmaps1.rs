fe9c6fd6001826642310c4b929cb056402937997
Andrea <freschiandrea86@gmail.com>
2023-02-18 17:10:41 +0100

    faccio i rustlings

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -10,17 +10,17 @@
 //
 // Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new();
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
+    basket.insert(String::from("ban"), 4);
+    basket.insert(String::from("banjna"), 6);
+    basket.insert(String::from("ban:wna"), 8);
 
-    // TODO: Put more fruits in your basket here.
 
     basket
 }
--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,23 +2,28 @@
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
+//
+//1. clonare oggetto quando perde ownership. ma costa in prestazioni e memoria OK
+//2. prestare possesso invece di passarlo alla funzione, e ritornare un oggetto di proprieta
+//   copiando l'originale
+//   OK
+//3. creare un tipo di riferimento mutabile, questo lavora direttamente sulla variabile originaria
+//   OK
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let vec1 = fill_vec(&vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
-    vec1.push(88);
+    vec0.push(88);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32>{
+    let mut vec = vec.clone();
 
     vec.push(22);
     vec.push(44);
